da42ba4c82cd85cc87bb605e9584b1d27ef8fc9f
Pranjal-Lohi <pranjallohi345@gmail.com>
2024-06-07 13:36:57 +0530

    updated the seed data

--- src/main/java/com/pranjal/SpringApplication/controller/HomeController.java
+++ src/main/java/com/pranjal/SpringApplication/controller/HomeController.java
@@ -1,5 +1,26 @@
 package com.pranjal.SpringApplication.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.pranjal.SpringApplication.models.Post;
+import com.pranjal.SpringApplication.services.PostService;
+
+
+
+@Controller
 public class HomeController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
+        return "home";
+    }
+  
     
 }
---
+++ src/main/java/com/pranjal/SpringApplication/services/PostService.java
@@ -0,0 +1,37 @@
+package com.pranjal.SpringApplication.services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pranjal.SpringApplication.models.Post;
+import com.pranjal.SpringApplication.repositories.PostRepository;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Optional<Post> getById(Long id){
+        return postRepository.findById(id);
+    }
+
+    public List<Post> getAll(){
+        return postRepository.findAll();
+    }
+
+    public void delete(Post post){
+        postRepository.delete(post);
+    }
+
+    public Post save(Post post){
+        if (post.getId() == null){
+            post.setCreatedAt(LocalDateTime.now());
+        }
+        return postRepository.save(post);
+    }
+}
+
