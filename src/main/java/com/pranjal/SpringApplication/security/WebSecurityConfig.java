29f4b070e8fb740a81279cf12c2d838445eab140
Pranjal-Lohi <pranjallohi345@gmail.com>
2024-06-10 19:57:04 +0530

    fixed some bugs

--- src/main/java/com/pranjal/SpringApplication/security/WebSecurityConfig.java
+++ src/main/java/com/pranjal/SpringApplication/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.pranjal.SpringApplication.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,56 +9,57 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.pranjal.SpringApplication.util.constants.Privillages;
-
+@Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableMethodSecurity
 public class WebSecurityConfig {
-    private static final String[] WHITELIST = {
-        "/",
-        "/login",
-        "/register",
-        "/db-console/**",
-        "/css/**",
-        "/fonts/**",
-        "/images/**",
-        "/js/**",
 
+    private static final String[] WHITELIST = {
+            "/",
+            "/login",
+            "/register",
+            "/db-console/**",
+            "/css/**",
+            "/fonts/**",
+            "/images/**",
+            "/js/**"
     };
 
     @Bean
-    public static PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @SuppressWarnings({ "deprecation", "removal" })
-	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests()
-        .requestMatchers(WHITELIST).permitAll()
-        .requestMatchers("/profile/**").authenticated()
-        .requestMatchers("/admin/**").hasRole("ADMIN")
-        .requestMatchers("/editor/**").hasAnyRole("ADMIN","EDITOR")
-        .requestMatchers("/test").hasAuthority(Privillages.ACCESS_ADMIN_PANEL.getPrivillage())
-        .and()
-        .formLogin()
-        .loginPage("/login").loginProcessingUrl("/login")
-        .usernameParameter("email").passwordParameter("password")
-        .defaultSuccessUrl("/", true).failureUrl("/login?error")
-        .permitAll()
-        .and()
-        .logout().logoutUrl("/logout")
-        .logoutSuccessUrl("/")
-        .and()
-        .httpBasic();
-
-        //TODO: remove these after upgrading the DB from H2 infile DB
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(WHITELIST).permitAll()
+                .requestMatchers("/profile/**").authenticated()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/editor/**").hasAnyRole("ADMIN", "EDITOR")
+                .anyRequest().authenticated()
+            )
+            .formLogin(formLogin -> formLogin
+                .loginPage("/login").loginProcessingUrl("/login")
+                .usernameParameter("email").passwordParameter("password")
+                .defaultSuccessUrl("/", true).failureUrl("/login?error")
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+            )
+            .rememberMe(rememberMe -> rememberMe
+                .rememberMeParameter("remember-me")
+            )
+            .httpBasic(withDefaults -> {})
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> {
+                headers.frameOptions(frameOptions -> frameOptions.sameOrigin());
+                headers.frameOptions(frameOptions -> frameOptions.disable());
+            });
 
         return http.build();
     }
-    
 }
-
